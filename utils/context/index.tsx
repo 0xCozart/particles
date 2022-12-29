d57d8e8929af3a1f7a04794ca7d6d107e1f11b47
alan <alanvega002@gmail.com>
2022-12-29 14:10:07 -0800

    create basicprofile hook

--- utils/context/index.tsx
+++ utils/context/index.tsx
@@ -1,16 +1,17 @@
+import { SelfID } from "@self.id/framework";
 import { AppContext } from "next/app";
 import { createContext, ReactNode } from "react";
 
 interface ContextType {
-  user: string | null;
-  login: () => void;
-  logout: () => void;
+  selfid: SelfID | null;
+  // login: () => void;
+  // logout: () => void;
 }
 
 const defaultContext: ContextType = {
-  user: null,
-  login: () => {},
-  logout: () => {},
+  selfid: null,
+  // login: () => {},
+  // logout: () => {},
 };
 
 const AppContext = createContext<ContextType>(defaultContext);
---
+++ utils/self-id/index.tsx
@@ -0,0 +1,78 @@
+import {
+  EthereumAuthProvider,
+  SelfID,
+  useViewerRecord,
+} from "@self.id/framework";
+import { WebClientSession } from "@self.id/web";
+import { useEffect, useState } from "react";
+
+async function UseBasicProfile() {
+  const record = useViewerRecord("basicProfile");
+  const [basicProfile, setBasicProfile] = useState<unknown>();
+  // not sure if this will properly update lmao
+  async function updateBasicProfile(fieldAndKey: object) {
+    if ((record.isMutable || !record.isMutating) && record.content) {
+      await record.merge({ ...record.content, ...fieldAndKey });
+    }
+  }
+  // useEffect to update context when basicProfle is updated
+  useEffect(() => {
+    if (!record.isLoading) {
+      setBasicProfile(record.content);
+    }
+  }, [record.isLoading, record.content]);
+
+  return [basicProfile, updateBasicProfile];
+}
+
+// This will change to check if did is authenticated and if session is live or expired
+function isSignedIn() {
+  return "did" in window ? true : false;
+}
+
+// doesnt need to return anything since useViewerConnection hook gives connection status
+async function ethereumSignIn(
+  connect: (provider: EthereumAuthProvider) => void
+) {
+  try {
+    if (!isSignedIn()) {
+      let accounts;
+      let selfId;
+      if (window.ethereum.request) {
+        accounts = await window.ethereum.request({
+          method: "eth_requestAccounts",
+        });
+      }
+
+      if (accounts) {
+        selfId = await connect(
+          new EthereumAuthProvider(window.ethereum, accounts[0])
+        );
+      }
+
+      return selfId;
+    }
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+function serializeSession(selfid: SelfID) {
+  // will most likely need a type check here
+  const session = (selfid.client as WebClientSession).session;
+  if (session) {
+    session.serialize();
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// async function ethereumSignOut(
+//   disconnect: () => void
+// ): Promise<ConnectionStatus> {
+//   await disconnect();
+//   return CONNECTION.idle;
+// }
+
+export { UseRecord, isSignedIn, ethereumSignIn, serializeSession };
